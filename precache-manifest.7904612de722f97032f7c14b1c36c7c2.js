46b8201c23225fbd1a4b215c904dcbfae94fbf5e
fatimaavilesfa <fatimaaviles1612@gmail.com>
2019-08-25 22:44:35 -0500

    Updates

---
+++ precache-manifest.7904612de722f97032f7c14b1c36c7c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38a98b81ad8c9c69aef5f1eb3cef35f",
+    "url": "/pruebaGhibli/index.html"
+  },
+  {
+    "revision": "78e782f3a67e1dceec73",
+    "url": "/pruebaGhibli/static/css/main.af941de6.chunk.css"
+  },
+  {
+    "revision": "9107d40b1b0086f16be5",
+    "url": "/pruebaGhibli/static/js/2.d7b41f65.chunk.js"
+  },
+  {
+    "revision": "78e782f3a67e1dceec73",
+    "url": "/pruebaGhibli/static/js/main.957922ff.chunk.js"
+  },
+  {
+    "revision": "a354cd392b6226c2ea60",
+    "url": "/pruebaGhibli/static/js/runtime~main.c40383ae.js"
+  }
+]);
